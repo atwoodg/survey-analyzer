113f8e4a7f2c865169efef766b45e5cb6a3062a1
Gabriel Atwood <gabrielatwood@uvic.ca>
2025-09-27 12:39:06 -0700

    Added lines to 2D array (excluding comments)

--- survey.c
+++ survey.c
@@ -1,13 +1,31 @@
-/* survey.c */
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-	char buffer[300];
 
-	printf("(Taking input from file) \n");
+    char buffer[1000];
+    char lines[30][1000];
+    int i = 0;
 
-	while(fgets(buffer, sizeof(buffer), stdin) !=NULL){
-		printf("%s", buffer);
-	}
-	return 0;
+    //while loop that gets each line in file and checks if it is a comment, if not, it adds it to the 2D array
+    while (fgets(buffer, 1000, stdin) != NULL) {
+        if (buffer[0] != '#') {
+            strcpy(lines[i], buffer);
+            i += 1;
+
+        }
+    }
+
+
+    for (int j = 0; j < i; j++) {
+        printf("%d\n", j);
+        printf("%s", lines[j]);
+    }
+
+    printf("%s", lines[12]);
+
+    return 0;
+
+
+
 }
